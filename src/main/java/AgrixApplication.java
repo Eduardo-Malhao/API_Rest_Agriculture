5abce976f34420b64ec346db3ceb94faf1b5749d
Eduardo-Malhao <eduardo.malhao@outlook.com>
2024-01-08 20:08:03 -0300

    docker funcionando

--- src/main/java/AgrixApplication.java
+++ src/main/java/AgrixApplication.java
@@ -1,5 +1,6 @@
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * Application main class. Start
@@ -7,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 //@EntityScan({"com.betrybe.agrix.ebytr.staff.entity"})
 //@EnableJpaRepositories({"com.betrybe.agrix.ebytr.staff.repository"})
-//@ComponentScan({"com.betrybe.agrix"})
+@ComponentScan("com.betrybe.agrix.src.main.java")
 public class AgrixApplication {
 
   public static void main(String[] args) {
